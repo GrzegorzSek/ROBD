3b579539d0d78fd35ef1e02f05ccee2bfcc5f3c7
GrzegorzSek <241778@student.pwr.edu.pl>
2021-11-24 10:10:43 +0100

    test

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+import sys
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
+    win.setWindowTitle("My first window!")
+
+    label = QLabel(win)
+    label.setText("my first label")
+    label.move(50, 50)
+
+    win.show()
+    sys.exit(app.exec_())
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main()
